92ca6635a4fd6f74da28ea76b1f10276b37f817f
Eleriin Kurs <eleriin.kurs@voco.ee>
2024-10-03 10:55:58 +0300

    Yl 3.3

---
+++ yl3.3.js
@@ -0,0 +1,12 @@
+const readline = require('node:readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.question("Taringute arv:",T2ringud=>{
+    for(let i=0; i<T2ringud;i++)
+console.log(Math.ceil(Math.random()*6))
+
+    rl.close()
+}
+)
